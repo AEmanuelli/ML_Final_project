2216a091c938f5db33ee16c7adde88ba5660b877
AEmanuelli <accent.cartilage0r@icloud.com>
2023-05-26 01:28:11 +0200

    Update Loss.py
    MSE

--- Code/Loss.py
+++ Code/Loss.py
@@ -4,3 +4,11 @@ class Loss(object):
 
     def backward(self, y, yhat):
         pass
+    
+   class MSELoss(Loss):
+    def forward(self, y, yhat):
+        return np.linalg.norm(y-yhat, axis=1) ** 2
+
+    def backward(self, y, yhat):
+        return -2*(y-yhat)
+
